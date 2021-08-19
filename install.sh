9be74aa11158235a38e630c50402d51670ab6fce
Saecki <tobiasschmitz2001@gmail.com>
2021-08-20 00:38:13 +0200

    install locally

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-cargo build --release
-sudo cp target/release/playlist-localizer /usr/local/bin
+cargo install --path .
 
 case "$SHELL" in
-    *bash)
-    echo "creating a completion script for bash"
-    /usr/local/bin/playlist-localizer -g "bash" | sudo tee /etc/bash_completion.d/playlist-localizer > /dev/null
-    ;;
     *zsh)
     echo "creating a completion script for zsh"
-    /usr/local/bin/playlist-localizer -g "zsh" | sudo tee /usr/share/zsh/site-functions/_playlist-localizer > /dev/null
+    ~/.cargo/bin/playlist-localizer -g "zsh" > ~/.config/zsh/functions/_playlist-localizer
     ;;
     *)
     echo "create a completion script for your shell manually by running 'playlist-localizer --generate-completion <shell>'"
